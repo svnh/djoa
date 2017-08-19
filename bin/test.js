36235bee8b61908f060e481e93159da163d1452a
alan345 <alan345@gmail.com>
2017-08-18 17:02:09 -0700

    test.js

--- bin/test.js
+++ bin/test.js
@@ -1,12 +1,11 @@
-var http = require('http');
-var server = http.createServer(function(request, response) {
-  response.writeHead(200,{"Content-Type":"text/plain"});
-  response.end('hello');
 
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
-server.listen(3000);
-console.log('server running');
-//https://www.youtube.com/watch?v=N3vgozk66dI
 
-//https://howtonode.org/how-to-install-nodejs
-//https://www.youtube.com/watch?v=N3vgozk66d
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
