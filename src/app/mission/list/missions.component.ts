214a0d0ab9f73ff8e96c98f3670222a5628ea97c
alan345 <alan345@gmail.com>
2018-02-14 10:19:36 -0800

    ok

--- src/app/mission/list/missions.component.ts
+++ src/app/mission/list/missions.component.ts
@@ -24,6 +24,7 @@ import { Categorie } from '../../categorie/categorie.model';
 })
 export class MissionsComponent implements OnInit {
   @Input() userId = '';
+  @Input() showHeader = true;
   @Input() categorie: Categorie = new Categorie();
   @Input() search: Search = new Search()
   @Output() getResultMissions: EventEmitter<any> = new EventEmitter();
