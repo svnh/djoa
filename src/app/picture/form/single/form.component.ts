d73f505423b337235bf62caad70d4c705e2e3765
alan345 <alan345@gmail.com>
2018-05-01 16:30:35 -0700

    ok

--- src/app/picture/form/single/form.component.ts
+++ src/app/picture/form/single/form.component.ts
@@ -124,7 +124,7 @@ export class FormComponent implements OnInit {
     }
   }
   isImagePure(file: File): boolean {
-    console.log(file.type)
+
     if (!file.type.match('image/*')) {
       return false;
     }
@@ -133,6 +133,7 @@ export class FormComponent implements OnInit {
 
   // check if the image is actually an image by checking the mime type
   isImage(file: File): boolean {
+    console.log(file.type)
     if (!file.type.match('image/*') && !file.type.match('application/pdf')) {
       this.toastr.error('Only images are allowed');
       return false;
