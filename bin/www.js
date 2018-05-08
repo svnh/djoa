864ad66a4e85dd15915ecf334b99f0ea44f4a42f
alan345 <alan345@gmail.com>
2018-05-08 15:57:59 -0700

    ok cert

--- bin/www.js
+++ bin/www.js
@@ -7,7 +7,7 @@
 var app = require('../server/app')
 var debug = require('debug')('petlocator_ng2:server')
 var https = require('https')
-var http = require('http')
+// var http = require('http')
 var fs = require('fs');
 
 
@@ -18,8 +18,8 @@ var fs = require('fs');
 //console.log(app.get('env'))
 
 
-// var port = normalizePort(process.env.PORT || '443')
-var port = normalizePort(process.env.PORT || '80')
+var port = normalizePort(process.env.PORT || '443')
+// var port = normalizePort(process.env.PORT || '80')
 
 
 app.set('port', port)
@@ -35,7 +35,7 @@ app.set('port', port)
    requestCert: false,
    rejectUnauthorized: false
  };
-var server = http.createServer( app)
+var server = https.createServer(sslOptions, app)
 
 /**
  * Listen on provided port, on all network interfaces.
