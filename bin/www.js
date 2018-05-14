ae4ce60de7f0fb3af14d01387327541c6beb03c8
alan345 <alan345@gmail.com>
2018-05-14 09:04:31 -0700

    server

--- bin/www.js
+++ bin/www.js
@@ -5,7 +5,7 @@
  */
 
 // Redirect from http port 80 to https
-var http = require('http');
+var http = require('http')
 http.createServer(function (req, res) {
   res.writeHead(301, { 'Location': 'https://' + req.headers.host + req.url })
   res.end()
