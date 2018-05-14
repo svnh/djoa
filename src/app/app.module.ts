bfd5d241c810738055c28c5c98d196ef59af04af
alan345 <alan345@gmail.com>
2018-05-14 09:55:31 -0700

    hammer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,6 +67,7 @@ import { CompanieGuardService} from './companie/companieGuard.service';
 
 import { AdminComponent} from './admin/admin.component';
 
+import 'hammerjs';
 
 export function authHttpServiceFactory(http: Http, options: RequestOptions) {
   return new AuthHttp( new AuthConfig({}), http, options);
