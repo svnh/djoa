babaf4393126e1c0ada605782fbc015a4d2b6767
alan345 <alan345@gmail.com>
2018-05-14 11:18:15 -0700

    ok

--- bin/www.js
+++ bin/www.js
@@ -5,11 +5,11 @@
  */
 
 // Redirect from http port 80 to https
-var http = require('http')
-http.createServer(function (req, res) {
-  res.writeHead(301, { 'Location': 'https://' + req.headers.host + req.url })
-  res.end()
-}).listen(80)
+  // var http = require('http')
+  // http.createServer(function (req, res) {
+  //   res.writeHead(301, { 'Location': 'https://' + req.headers.host + req.url })
+  //   res.end()
+  // }).listen(80)
 // Redirect from http port 80 to https
 
 var app = require('../server/app')
