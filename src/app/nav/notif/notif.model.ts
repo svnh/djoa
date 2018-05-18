bbee510bf72112a3efc24e14c420a090d030b35d
DorianeM <doriane@latelier-marketing.com>
2018-05-17 18:51:50 -0700

    notif mod

--- src/app/nav/notif/notif.model.ts
+++ src/app/nav/notif/notif.model.ts
@@ -1,8 +1,6 @@
 
 import { Mission } from '../../mission/mission.model';
 import { Document } from '../../document/document.model';
-import { Form } from '../picture/form/form.model';
-
 
 export class NotifChat {
   mission: Mission = new Mission();
